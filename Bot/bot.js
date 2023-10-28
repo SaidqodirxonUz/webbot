825c0bda43d253fcaafc83019f7c270c93ef0111
SaidqodirxonUz <SaidqodirxonUz@gmail.com>
2023-10-28 23:46:02 +0500

    is ready .

--- Bot/bot.js
+++ Bot/bot.js
@@ -3,12 +3,12 @@ import { Telegraf, Markup } from "telegraf";
 const TOKEN = "5392304585:AAHLbeU2V-ZP4CRu2US1aeEDZ_CpFXlPDx4";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "http://127.0.0.1:5173/";
+const web_link = "https://verdant-dango-505183.netlify.app/";
 
 const webAppButton = Markup.button.url("Open Web App", web_link);
 
 bot.start((ctx) => {
-  ctx.reply("Welcome :))))");
+  ctx.reply("Welcome :) \n\n /web - web-link");
 });
 
 bot.command("web", (ctx) => {
