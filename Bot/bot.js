86b57aa9a00173a23c1c1139296e88996b86edff
SaidqodirxonUz <SaidqodirxonUz@gmail.com>
2023-10-30 22:59:48 +0500

    id oladigan qildim

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,18 +1,35 @@
 import { Telegraf, Markup } from "telegraf";
 
-const TOKEN = "5392304585:AAHLbeU2V-ZP4CRu2US1aeEDZ_CpFXlPDx4";
+// const TOKEN = "5392304585:AAHLbeU2V-ZP4CRu2US1aeEDZ_CpFXlPDx4"; ///@SaidqodirxonUzBot
+const TOKEN = "5918611696:AAFxqlcvrKJJWA1G5VsBYf_PQ0edM8uGK6Y"; ///@SertifikatProBot
+
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://verdant-dango-505183.netlify.app/";
+// const web_link = "https://verdant-dango-505183.netlify.app/";
+const web_link = "https://693d-84-54-73-69.ngrok-free.app";
 
-const webAppButton = Markup.button.url("Open Web App", web_link);
+const menu = Markup.keyboard([["Interaktiv Menyu"]])
+  .oneTime()
+  .resize();
 
 bot.start((ctx) => {
-  ctx.reply("Welcome :) \n\n /web - web-link");
+  ctx.reply("Hush kelibsiz :)\n\n\n /web - Interaktiv Menyu", menu);
 });
 
 bot.command("web", (ctx) => {
-  ctx.reply("web", Markup.inlineKeyboard([[webAppButton]]));
+  const webAppButton = Markup.button.webApp("Interaktiv Menyu", web_link);
+  ctx.reply("Interaktiv Menyu", Markup.inlineKeyboard([[webAppButton]]));
+});
+
+bot.hears("Interaktiv Menyu", (ctx) => {
+  ctx.reply(
+    "Interaktiv Menyu",
+    Markup.inlineKeyboard([
+      [Markup.button.webApp("Interaktiv Menyu", web_link)],
+    ])
+  );
 });
 
 bot.launch();
+
+console.log("Bot ishladi");
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
 import "./App.css";
 import Card from "./Components/Card/Card";
@@ -5,14 +7,29 @@ import Cart from "./Components/Cart/Cart";
 import { getData } from "./db/db";
 const foods = getData();
 
+const botToken = "5918611696:AAFxqlcvrKJJWA1G5VsBYf_PQ0edM8uGK6Y";
+
 function App() {
   const [cartItems, setCartItems] = useState([]);
+  const [userChatId, setUserChatId] = useState(null);
 
   useEffect(() => {
     if (window.Telegram) {
-      window.Telegram.WebApp.ready();
+      const tele = window.Telegram.WebApp;
+      tele.ready();
+      tele.enableClosingConfirmation();
+
+      if (tele.initDataUnsafe) {
+        setUserChatId(tele.initDataUnsafe.user.id);
+
+        console.log(userChatId);
+      } else {
+        console.error("Telegram initDataUnsafe is not available.");
+      }
+    } else {
+      console.error("Telegram WebApp is not available.");
     }
-  });
+  }, []);
 
   const onAdd = (food) => {
     const exist = cartItems.find((x) => x.id === food.id);
@@ -29,6 +46,7 @@ function App() {
 
   const onRemove = (food) => {
     const exist = cartItems.find((x) => x.id === food.id);
+
     if (exist.quantity === 1) {
       setCartItems(cartItems.filter((x) => x.id !== food.id));
     } else {
@@ -40,9 +58,60 @@ function App() {
     }
   };
 
+  // const onCheckout = () => {
+  //   // Check if Telegram WebApp is available
+  //   if (Telegram && Telegram.WebApp) {
+  //     const tele = Telegram.WebApp;
+  //     tele.MainButton.text = "Pay :)";
+  //     tele.MainButton.show();
+  //   }
+  // };
+
   const onCheckout = () => {
-    tele.MainButton.text = "Pay :)";
-    tele.MainButton.show();
+    if (Telegram && Telegram.WebApp) {
+      const tele = Telegram.WebApp;
+
+      tele.MainButton.text = `Pay :) ${userChatId}`;
+      tele.MainButton.show();
+
+      // Define a function to format cart items as needed
+      const formatCartItems = () => {
+        return cartItems
+          .map((item) => {
+            return `${item.quantity} x ${item.name} - $${
+              item.price * item.quantity
+            }`;
+          })
+          .join("\n");
+      };
+
+      console.log(tele, "tele");
+
+      tele.MainButton.onClick(() => {
+        const formattedCartItems = formatCartItems();
+        const message = "ha oxshadi ";
+
+        console.log("Main Buttonga kirdi ");
+        console.log(userChatId);
+
+        function sendMessageToUser(message) {
+          var data = {
+            action: "sendMessage",
+            payload: {
+              chat_id: userChatId,
+              text: message,
+            },
+          };
+
+          window.Telegram.WebApp.sendData(JSON.stringify(data));
+        }
+        x;
+        sendMessageToUser(message);
+        console.log(sendMessageToUser);
+      });
+    } else {
+      console.error("Telegram WebApp is not available.");
+    }
   };
 
   return (
